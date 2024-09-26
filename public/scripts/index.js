f6448d8a4f2ef97370ddc0bfaa70248bdec1364f
tom-sebastian-67 <22b490@gcek.ac.in>
2024-09-26 18:12:48 +0530

    added clock with full functionality

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,8 +5,33 @@ $(document).ready(function () {
     $('html, body').animate({
         scrollTop: -200 // Change this value to scroll to a specific position
     }, 300);
-
+    startCurrentTime();
 });
+
+var timeLabel = $("#current-time");
+
+    function startCurrentTime() {
+    let currentDate = new Date();
+    let day = currentDate.getDate();
+    let month = currentDate.getMonth() + 1; // Months are 0-indexed, so add 1
+    let year = currentDate.getFullYear();
+
+
+    let formattedDate = `${day}-${month}-${year}`;
+    let dateLabel = $("#date"); 
+    dateLabel.text(formattedDate);
+
+    setInterval(() => {
+        let currentTime = new Date();
+        var hour = currentTime.getHours();
+        if(hour > 12) {
+            hour -= 12;
+        }
+        var minute = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+        timeLabel.text(`${hour}:${minute}:${seconds}`);
+    },1000);
+}
 
 
 setInterval(() => {
@@ -47,8 +72,9 @@ $('button').click(function () {
             top: '180px'
         }, 600);
         isUp = false;
+        $("button").text("Timer");
     } else {
-        
+       
         $('html, body').animate({
             scrollTop: -900 
         }, 600);
@@ -57,5 +83,8 @@ $('button').click(function () {
             top: '-150px'
         }, 600);
         isUp = true;
+        $("button").text("clock");
     }
 });
+
+
